72b7a265d04e165f9e6349840deabb6ccab3eeeb
kolawoleajibola <pricelesspropertiesservices@gmail.com>
2022-07-08 13:09:02 -0700

    removing the loop code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i;
-
+/* */
 printf("Iinfinite loop incoming :(\n");
 
 i = 0;
@@ -17,8 +17,7 @@ i = 0;
 * {
 * putchar(i);
 *}
-*/
-printf("Infinite loop avoided! \\o/\n");
+*/printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
